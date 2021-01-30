5fff02465e88fe32f968b02b6c704aab64eaee9a
Yuzq1226 <yuzq12@sina.com>
2021-01-30 16:10:22 +0800

    2021-1-30 3:00pm

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    html = "<a href=\'/rango/about\'>About</a>"
+    return HttpResponse(html)
+
+def about(request):
+    html = "<a href=\'/rango/\'>Index</a>"
+    return HttpResponse(html)
