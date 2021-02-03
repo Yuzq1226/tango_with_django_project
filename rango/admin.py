0721a9968bc844ff9ffcf8af35a63845c5f95047
Yuzq1226 <yuzq12@sina.com>
2021-02-03 19:25:16 +0800

    2021-2-3 7:30pm by yzq

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from rango.models import Category, Page, PageAdmin
+from rango.models import UserProfile
 
 # Add in this class to customise the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,3 +10,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
